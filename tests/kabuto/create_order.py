25393802f2aa268472a58820dc0c3ed0849c4b14
kokimame <kohkimametani@gmail.com>
2022-04-23 20:51:22 +0900

    Try createOrder

---
+++ tests/kabuto/create_order.py
@@ -0,0 +1,15 @@
+import ccxt
+
+from freqtrade.kabuto.credentials import KabutoCredential as kCred
+
+print(ccxt.__version__)
+
+kabus = ccxt.kabus({
+    'ipaddr': kCred.host_ipaddr,
+    'password': kCred.password_live,
+    'kabusapi_password': kCred.kabusapi_password,
+})
+
+response = kabus.create_order('9318@1/JPY', 'limit', 'buy', 100, 1)
+
+print(response)
