7b2adafef9a6d113aae7c2d5666981c1d492f434
kokimame <kohkimametani@gmail.com>
2022-04-23 18:42:25 +0900

    Update credentials

--- freqtrade/kabuto/credentials.py
+++ freqtrade/kabuto/credentials.py
@@ -5,22 +5,26 @@ from dataclasses import dataclass
 
 @dataclass
 class Credential:
-    password: str
-    id: str
+    kabusapi_password: str
+    kabusapi_id: str
     host_ipaddr: str
     host_live: str
+    host_test: str
     password_live: str
+    password_test: str
     onetime_token: str
 
 
 with open(f'{os.environ["HOME"]}/.kabuto/credentials.json') as f:
-    data = json.load(f)
+    CredentialRaw = json.load(f)
 
 KabutoCredential = Credential(
-    password=data['KABUCOM_PASSWORD'],
-    id=data['KABUSAPI_ID'],
-    host_ipaddr=data['KABUSAPI_HOST'],
-    host_live=data['KABUSAPI_HOST_LIVE'],
-    password_live=data['KABUSAPI_PASSWORD_LIVE'],
-    onetime_token=data['KABUSAPI_ONETIME_TOKEN']
+    kabusapi_password=CredentialRaw['KABUCOM_PASSWORD'],
+    kabusapi_id=CredentialRaw['KABUSAPI_ID'],
+    host_ipaddr=CredentialRaw['KABUSAPI_HOST'],
+    host_live=CredentialRaw['KABUSAPI_HOST_LIVE'],
+    host_test=CredentialRaw['KABUSAPI_HOST_TEST'],
+    password_live=CredentialRaw['KABUSAPI_PASSWORD_LIVE'],
+    password_test=CredentialRaw['KABUSAPI_PASSWORD_TEST'],
+    onetime_token=CredentialRaw['KABUSAPI_ONETIME_TOKEN']
 )
