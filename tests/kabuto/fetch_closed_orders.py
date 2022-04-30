739fe445d5129caee56b0fffaa92b9978ef61046
kokimame <kohkimametani@gmail.com>
2022-04-30 20:07:35 +0900

    Added fetch{Open/Closed}Orders

---
+++ tests/kabuto/fetch_closed_orders.py
@@ -0,0 +1,15 @@
+import ccxt
+
+from freqtrade.kabuto.credentials import KabutoCredential as kCred
+
+print(ccxt.__version__)
+
+kabus = ccxt.kabus({
+    'ipaddr': kCred.host_ipaddr,
+    'password': kCred.password_live,
+    'kabusapi_password': kCred.kabusapi_password
+})
+
+orders = kabus.fetch_closed_orders()
+
+print(orders)
